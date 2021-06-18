a2dd74db514411dc9fbe36601b9eb906eeaf9729
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2021-06-18 22:24:30 +0800

    Initial commit

---
+++ src/main/java/come/ken/Person.java
@@ -0,0 +1,7 @@
+package come.ken;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/kotlin/com/ken/Hello.kt
@@ -0,0 +1,15 @@
+package com.ken
+
+fun main() {
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello kotlin")
+    }
+
+}
